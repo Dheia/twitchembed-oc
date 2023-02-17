460512641fd68c73713e8f9c5e6f82f8280afc35
NielsvandenDries <niels@openmindedit.nl>
2023-02-17 23:27:23 +0100

    Added VOD section backend

---
+++ updates/builder_table_create_nielsvandendries_twitchembed_vod.php
@@ -0,0 +1,27 @@
+<?php namespace NielsVanDenDries\Twitchembed\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateNielsvandendriesTwitchembedVod extends Migration
+{
+    public function up()
+    {
+        Schema::create('nielsvandendries_twitchembed_vod', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('video');
+            $table->smallInteger('height');
+            $table->smallInteger('width');
+            $table->string('parent');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('nielsvandendries_twitchembed_vod');
+    }
+}
